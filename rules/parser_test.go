9669b3a67d27b59c8832592366dd75b8afc2b7f1
Marcel <marcel@mahillmann.de>
2019-12-30 18:33:59 +0100

    bugfix unite Tests Windows2Linux
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- rules/parser_test.go
+++ rules/parser_test.go
@@ -36,7 +36,7 @@ func rulesParserFailOpen(t *testing.T) {
 		require.NotNil(t, err)
 		sErr := err.(error).Error()
 		require.Condition(t, func() bool {
-			return strings.HasPrefix(sErr, "SEVERE  [mailAssistant.rules.parseYaml#lambda] open ../testdata/rules/notExists.yml:")
+			return strings.HasPrefix(sErr, "open ../testdata/rules/notExists.yml:")
 		}, sErr)
 	}()
 
