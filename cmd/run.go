eb29403db5fcc9cdb9a78d067e303810364e1abb
Marcel <marcel@mahillmann.de>
2020-01-02 19:03:53 +0100

    add entrypoint
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- cmd/run.go
+++ cmd/run.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/urfave/cli/v2"
+	"log"
 	"mailAssistant/account"
 	"mailAssistant/cntl"
 	"mailAssistant/rules"
@@ -11,16 +12,20 @@ import (
 
 // RunAssistant is execute the main logic
 func RunAssistant(c *cli.Context) error {
+	log.Print(">> RunAssistant")
 	_ = c.String("config")
 
 	if accounts, err := account.ImportAccounts(); err != nil {
+		log.Print("<< RunAssistant",err)
 		return err
 	} else if err := rules.ImportAndLaunch(accounts); err != nil {
+		log.Print("<< RunAssistant",err)
 		return err
 	} else {
 		cntl.WaitForOsNotify(os.Interrupt, syscall.SIGTERM)
 		cntl.WaitForNotify()
 		cntl.StopAllClocks()
 	}
+	log.Print("<< RunAssistant nil")
 	return nil
 }
