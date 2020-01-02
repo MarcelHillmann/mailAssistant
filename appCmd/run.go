279c60d103a2b8ec40b9be4a41ed7be77fc759bf
Marcel <marcel@mahillmann.de>
2020-01-02 15:23:42 +0100

    add urfave/cli
    Signed-off-by: Marcel <marcel@mahillmann.de>

---
+++ appCmd/run.go
@@ -0,0 +1,23 @@
+package appCmd
+
+import (
+	"github.com/urfave/cli/v2"
+	"mailAssistant/account"
+	"mailAssistant/cntl"
+	"mailAssistant/rules"
+	"os"
+	"syscall"
+)
+
+func RunAssistant(c *cli.Context) error {
+	if accounts, err := account.ImportAccounts(); err != nil {
+		return err
+	} else if err := rules.ImportAndLaunch(accounts); err != nil {
+		return err
+	} else {
+		cntl.WaitForOsNotify(os.Interrupt, syscall.SIGTERM)
+		cntl.WaitForNotify()
+		cntl.StopAllClocks()
+	}
+	return nil
+}
